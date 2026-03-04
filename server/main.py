14ec25ec958034e0bd4ee762aee65af42c421fe6
Euan Jones <euanajones@gmail.com>
2026-03-04 23:33:39 +0000

    Added CORS middleware for future React integration

--- server/main.py
+++ server/main.py
@@ -1,6 +1,21 @@
+import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
@@ -9,3 +24,6 @@ def root():
 @app.get("/user/{user_name}")
 def welcome_user(user_name: str):
     return {"message": f"Welcome, {user_name}!"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)

3e887c4c81b7ccca23258041dc286a5df75a7323
Euan Jones <euanajones@gmail.com>
2026-03-07 16:17:00 +0000

    Set up SQLite db connection

--- server/main.py
+++ server/main.py
@@ -1,6 +1,8 @@
+from sqlmodel import create_engine, Session, SQLModel
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import bcrypt
 
 app = FastAPI()
 
@@ -17,13 +19,31 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/")
-def root():
-    return {"message": "Hello World"}
+sqlite_file_name = "test_database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
 
-@app.get("/user/{user_name}")
-def welcome_user(user_name: str):
-    return {"message": f"Welcome, {user_name}!"}
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+def create_tables():
+    SQLModel.metadata.create_all(engine)
+
+def hash_password(password: str) -> str:
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(password, salt)
+    return hashed
+
+@app.get("/health")
+def health_check():
+    return {"status": "Running"}
+
+@app.on_event("startup")
+def on_startup():
+    create_tables()
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)

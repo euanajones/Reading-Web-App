d5285c0a182647234806bf633a3a3b1563b4d98a
Euan Jones <euanajones@gmail.com>
2026-03-11 21:47:38 +0000

    Set out base dashboard design

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import AuthPages from './auth/AuthPages'
 import TestPage from './pages/Test';
+import UserPages from './pages/UserPages';
 
 function App() {
   return (
     <>
       <AuthPages />
       <TestPage />
+      <UserPages />
     </>
   )
 }
---
+++ client/src/pages/UserPages.jsx
@@ -0,0 +1,25 @@
+import { Routes, Route, Link } from 'react-router-dom';
+
+function Dashboard() {
+    return (
+        <>
+            <h1>User's Bookshelf</h1>
+            <p>Welcome to your dashboard!</p>
+            <hr />
+            <p>Currently Reading</p>
+            <hr />
+            <p>Pick Up Where You Left Off</p>
+            <hr />
+            <p>Search Your Library</p>
+            
+        </>
+    );
+}
+
+export default function UserPages() {
+    return (
+        <Routes>
+            <Route path="/home" element={<Dashboard />} />
+        </Routes>
+    );
+}
